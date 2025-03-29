e2b4e29d316f9e0e663e122d169f393150809e5b
unknown <jesuschetia.co@gmail.com>
2025-03-29 19:51:05 +0530

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
