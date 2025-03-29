d3f4ed41d9e6fc4ab7b2e69218fac0ffaddc76a9
unknown <jesuschetia.co@gmail.com>
2025-03-29 15:14:34 +0530

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [react()],
     output: 'server',
+    adapter: netlify({
+        edgeMiddleware: true
+    }),
+    integrations: [react()],
 });
